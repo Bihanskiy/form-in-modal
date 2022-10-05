d9a2dc466c92e94e4139d853b329a30f8cb1d64a
Bihanskiy <bohdan.bihanskiy@gmail.com>
2022-10-06 00:18:11 +0300

    update validation

--- src/js/features/validation.js
+++ src/js/features/validation.js
@@ -8,28 +8,28 @@ function setValidation() {
         form = id("form"),
         errorMsg = classes("form__error"),
         successMsg = classes("form__success");
-        console.log(successMsg);
 
-    const validator = (id, serial, message) => {
-        if (id.value.trim() === "") {
-            errorMsg[serial].innerHTML = message;
-            id.style.border = "2px solid red";
-        } else if (id.value.length < 2) {
+    const validator = (id, serial) => {
+        if (id.value.length < 2) {
             errorMsg[serial].innerHTML = "Value must be more than 2 symbols";
             id.style.border = "2px solid red";
+            return false;
         } else {
             errorMsg[serial].innerHTML = "";
             id.style.border = "2px solid green";
-            successMsg[0].innerHTML = "Form submitted successfully";
+            return true;
         }
     };
+
+    const setValidate = (...args) => {
+        return args.every(inputValidation => inputValidation === true)
+    }
 
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        validator(username, 0, "Enter user name");
-        validator(email, 1, "Enter email");
-        validator(password, 2, "Enter phone number");
+        const globalValidation = setValidate(validator(username, 0), validator(email, 1), validator(password, 2));
+        globalValidation ? successMsg[0].innerHTML = "Form submitted successfully" : successMsg[0].innerHTML = "";
     });
 }
 
